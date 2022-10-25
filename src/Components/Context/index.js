67b45a365493876992c78e59454048890dc646dd
Neil Francis Hipona <neil.francis.hipona@codeandtheory.com>
2022-10-25 11:38:22 +0800

    Update .gitignore && add giphy key

--- src/Components/Context/index.js
+++ src/Components/Context/index.js
@@ -4,7 +4,7 @@ export const EnvironmentContext = React.createContext();
 
 export const EnvironmentProvider = (props) => {
     const config = {
-        API_KEY: '',
+        API_KEY: 'Z2yj1zlW53iWFjqS3kvp4m9nLSJhyvd6',
     };
 
     return (
