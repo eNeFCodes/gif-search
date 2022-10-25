6172aa29968c4c80f60a14ef88202b5585bc9828
Neil Francis Hipona <neil.francis.hipona@codeandtheory.com>
2022-10-25 11:35:53 +0800

    Updates

---
+++ precache-manifest.3f9eee907386f54e190081beb01f39f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d77489c47ccded840101c7320a6be54",
+    "url": "/gif-search/index.html"
+  },
+  {
+    "revision": "42f2ae03db961c084dd5",
+    "url": "/gif-search/static/css/main.7a0e6566.chunk.css"
+  },
+  {
+    "revision": "f06734d821875aeb5ddf",
+    "url": "/gif-search/static/js/2.ba3d4cdd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/gif-search/static/js/2.ba3d4cdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42f2ae03db961c084dd5",
+    "url": "/gif-search/static/js/main.6aac4128.chunk.js"
+  },
+  {
+    "revision": "6b56c2132094f422e07e",
+    "url": "/gif-search/static/js/runtime-main.7ad2b75a.js"
+  }
+]);
